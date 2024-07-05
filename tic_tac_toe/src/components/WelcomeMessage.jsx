e607e587c48935d10c05867dd14226000e07c5a5
Saloni-16M <156694519+Saloni-16M@users.noreply.github.com>
2024-07-05 21:37:48 +0530

    Delete tic_tac_toe directory

--- tic_tac_toe/src/components/WelcomeMessage.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { TodoItemsContext } from "../store/todo-items-store";
-
-const WelcomeMessage = () => {
-  const todoItems=useContext(TodoItemsContext)
-  return(todoItems.length===0 &&
-  <p>Enjoy Your Day</p>);
-};
-export default WelcomeMessage;
